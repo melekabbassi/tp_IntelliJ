99fb812a6eec7c0cad9f581095d2c31db0a6c065
melekabbassi <abbassimelek@gmail.com>
2022-10-25 19:41:17 +0100

    changed password

--- src/main/java/Connection.java
+++ src/main/java/Connection.java
@@ -2,17 +2,21 @@ import java.sql.*;
 
 public class Connection {
     public Connection() throws SQLException {
-String url = "jdbc:mariadb://localhost:3306/gestion_de_note";
+        String url = "jdbc:mariadb://localhost:3306/gestion_de_note";
         String user = "octopus";
-        String password = "Benalidegage_23";
+        String password = "testPass";
 
         try {
             Class.forName("com.mysql.jdbc.Driver");
+            java.sql.Connection connection = DriverManager.getConnection(url, user, password);
+            Statement statement = connection.createStatement();
+
+            // Create table client based on the client class
+            statement.executeUpdate("CREATE TABLE IF NOT EXISTS client (id INT AUTO_INCREMENT PRIMARY KEY, nom VARCHAR(255), prenom VARCHAR(255), email VARCHAR(255), telephone VARCHAR(255), adresse VARCHAR(255))");
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
 
-        java.sql.Connection connection = DriverManager.getConnection(url, user, password);
-        Statement statement = connection.createStatement();
+
     }
 }
